652b404eb8fed93a08879607b55f9f637c9b2656
River-Walras <nachuan.shi.quant@gmail.com>
2026-01-28 15:04:18 +0800

    Fix CI wheel build

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 import subprocess
 import sys
 
@@ -11,8 +12,15 @@ class BuildPyCommand(_build_py):
         # Then run the normal build
         super().run()
 
+class BDistWheelCommand(_bdist_wheel):
+    """Mark wheel as non-pure (platform-specific) since we ship compiled extensions."""
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+
 setup(
     cmdclass={
         'build_py': BuildPyCommand,
+        'bdist_wheel': BDistWheelCommand,
     },
 )
